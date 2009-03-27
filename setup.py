32d720838b11c75dd0e1cfa53e9ecfdedaffb1fd
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-03-27 22:47:20 +1100

    better name

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 
 from setuptools import *
 setup(
-	name='rednose plugin for nosetests',
+	name='rednose',
+	description="coloured output for nosetests",
 	packages = find_packages(),
 	entry_points = {
 		'nose.plugins.0.10': ['rednose = rednose:RedNose']
