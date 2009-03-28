d6c6ddd716a388d4da7fda610f6c371b0839692e
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-03-28 13:57:06 +1100

    fleshed out the setup script

--- setup.py
+++ setup.py
@@ -3,9 +3,26 @@
 from setuptools import *
 setup(
 	name='rednose',
+	version='0.1',
+	author_email='tim3d.junk+termstyle@gmail.com',
+	author='Tim Cuthbertson',
+	url='http://github.com/gfxmonk/rednose/tree',
 	description="coloured output for nosetests",
 	packages = find_packages(),
 	entry_points = {
 		'nose.plugins.0.10': ['rednose = rednose:RedNose']
 	},
+	classifiers=[
+		"License :: OSI Approved :: BSD License",
+		"Programming Language :: Python",
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Software Development :: Testing",
+	],
+	keywords='console ansi color colour terminal xterm',
+	license='BSD',
+	install_requires=[
+		'setuptools',
+	],
 )
